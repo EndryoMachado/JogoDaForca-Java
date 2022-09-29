36369d7842dc9304577983ffad85634bbcd1aee4
EndryoMachado <89991280+EndryoMachado@users.noreply.github.com>
2022-09-28 22:33:53 -0300

    Update JogoDaForca.java

--- JogoDaForca.java
+++ JogoDaForca.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JogoDaForca
+public class Main
 {
 	public static void main(String[] args) {
 	    
@@ -14,29 +14,31 @@ public class JogoDaForca
 	        asteristicos += "*";
 	    }
 	    System.out.println(palavra);
-	    
+	    clearConsole();
 	    while(chance != 0){
+	        
 	        System.out.println("Chances: " + chance);
 	        System.out.println(asteristicos);
 	        letra = input.next().charAt(0);
 	        
 	        StringBuilder newString = new StringBuilder(asteristicos);
 	        for(int i = 0; i < palavra.length(); i++){
-	            if(palavra.charAt(i) == letra){
+	            if(palavra.charAt(i) == letra && asteristicos.contains(""+letra) == false){
 	                newString.setCharAt(i, letra);
 	            }
 	        }
 	        
-	        if(asteristicos.equals(""+newString)){
+	        if(asteristicos.equals(""+newString) && asteristicos.contains(""+letra) == false){
 	            chance--;
 	            if(chance == 0){
-	                System.out.println("Você perdeu");
+	                System.out.println("Você perdeu.\nA palavra era: "+palavra);
 	            }
 	        }else{
 	            asteristicos = ""+newString;
 	        }
 	        
 	        if(asteristicos.equals(palavra)){
+	            System.out.println(asteristicos);
 	            System.out.println("Você venceu");
 	            break;
 	        }
